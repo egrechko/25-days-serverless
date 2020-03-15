5fe4746767a9170fd878d830e799c65c9b36a1ff
Eugene Grechko <eugene.grechko@gmail.com>
2020-03-15 13:39:04 -0700

    Added latest files from desktop.

--- day3/index.js
+++ day3/index.js
@@ -1,6 +1,6 @@
 module.exports.hello = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify('Welcome to the users API.')
+    body: JSON.stringify('Welcome to the users API. View documentation at docs.webapp.io')
   }
 }
